40156504432a9fa628cacaea361e66a90d7cacf5
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-03-16 21:38:34 +0100

    Database testing

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require("dotenv").config();
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(cors());
+
+MongoClient.connect(process.env.MONGODB_URI, {
+  useUnifiedTopology: true,
+})
+  .then((client) => {
+    console.log("Databasen är ok");
+
+    const db = client.db("projekt1");
+    app.locals.db = db;
+  })
+  .catch((error) => console.error(error));
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  req.app.locals.db
+    .collection("users")
+    .find()
+    .toArray()
+    .then((results) => {
+      console.log(results);
+
+      let printUsers = "<div><h2>Våra users</h2>";
+
+      for (user in results) {
+        printUsers += `<div>${results[user].name}</div>`;
+      }
+
+      printUsers += "</div>";
+
+      res.send(printUsers);
+    });
+});
+
+router.post("/add", function (req, res, next) {
+  const newUser = req.body;
+
+  req.app.locals.db
+    .collection("users")
+    .insertOne(newUser)
+    .then((result) => {
+      console.log("resultat från db: ", result);
+      res.json(result);
+    })
+    .catch((err) => console.error(err));
 });
 
 module.exports = router;
